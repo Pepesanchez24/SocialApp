d9fe797a73e22bd1cb2d2b56811f64997e8aa017
Marc <masape@elpuig.xeill.net>
2025-03-25 17:00:10 +0100

    Projecto

---
+++ app/src/main/java/com/example/socialapp/HomeFragment.java
@@ -0,0 +1,223 @@
+package com.example.socialapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.appwrite.Client;
+import io.appwrite.coroutines.CoroutineCallback;
+import io.appwrite.exceptions.AppwriteException;
+import io.appwrite.models.DocumentList;
+import io.appwrite.services.Account;
+import io.appwrite.services.Databases;
+
+public class HomeFragment extends Fragment {
+
+    AppViewModel appViewModel;
+    NavController navController;
+    PostsAdapter adapter;
+    ImageView photoImageView;
+    TextView displayNameTextView, emailTextView;
+    Client client;
+    Account account;
+    String userId;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle
+            savedInstanceState) {
+        NavigationView navigationView = view.getRootView().findViewById(R.id.nav_view);
+        View header = navigationView.getHeaderView(0);
+        photoImageView = header.findViewById(R.id.imageView);
+        displayNameTextView = header.findViewById(R.id.displayNameTextView);
+        emailTextView = header.findViewById(R.id.emailTextView);
+        client = new Client(requireContext()).setProject(getString(R.string.APPWRITE_PROJECT_ID));
+        account = new Account(client);
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        try {
+            account.get(new CoroutineCallback<>((result, error) -> {
+                if (error != null) {
+                    error.printStackTrace();
+                    return;
+                }
+                mainHandler.post(() ->
+                {
+                    userId = result.getId();
+                    displayNameTextView.setText(result.getName().toString());
+                    emailTextView.setText(result.getEmail().toString());
+                    Glide.with(requireView()).load(R.drawable.user).into(photoImageView);
+                    obtenerPosts();
+                });
+            }));
+        } catch (AppwriteException e) {
+            throw new RuntimeException(e);
+        }
+
+        navController = Navigation.findNavController(view);
+        RecyclerView postsRecyclerView = view.findViewById(R.id.postsRecyclerView);
+        adapter = new PostsAdapter();
+        postsRecyclerView.setAdapter(adapter);
+
+        view.findViewById(R.id.gotoNewPostFragmentButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.fragment_new_post);
+            }
+        });
+    }
+
+    public HomeFragment() {
+    }
+
+    class PostViewHolder extends RecyclerView.ViewHolder{
+        ImageView authorPhotoImageView, likeImageView, mediaImageView;
+        TextView authorTextView, contentTextView, numLikesTextView;
+        PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            authorPhotoImageView = itemView.findViewById(R.id.photoImageView);
+            likeImageView = itemView.findViewById(R.id.likeImageView);
+            mediaImageView = itemView.findViewById(R.id.mediaImage);
+            authorTextView = itemView.findViewById(R.id.authorTextView);
+            contentTextView = itemView.findViewById(R.id.contentTextView);
+            numLikesTextView = itemView.findViewById(R.id.numLikesTextView);
+        }
+    }
+
+    class PostsAdapter extends RecyclerView.Adapter<PostViewHolder> {
+        DocumentList<Map<String,Object>> lista = null;
+        // Miniatura de media
+
+
+        @Override
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int
+                viewType) {
+            return new
+                    PostViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_viewholder_post, parent, false));
+        }
+        @SuppressLint("SuspiciousIndentation")
+        @Override
+        public void onBindViewHolder(@NonNull PostViewHolder holder, int position)
+        {
+            Map<String,Object> post = lista.getDocuments().get(position).getData();
+
+            if (post.get("mediaUrl") != null) {
+                holder.mediaImageView.setVisibility(View.VISIBLE);
+                if ("audio".equals(post.get("mediaType").toString())) {
+
+                    Glide.with(requireView()).load(R.drawable.audio).centerCrop().into(holder.mediaImageView);
+                } else {
+
+                    Glide.with(requireView()).load(post.get("mediaUrl").toString()).centerCrop().into (holder.mediaImageView);
+                }
+                holder.mediaImageView.setOnClickListener(view -> {
+                    appViewModel.postSeleccionado.setValue(post);
+                    navController.navigate(R.id.fragment_Media);
+                });
+            } else {
+                holder.mediaImageView.setVisibility(View.GONE);
+            }
+
+            holder.authorTextView.setText(post.get("author").toString());
+            holder.contentTextView.setText(post.get("content").toString());
+
+            List<String> likes = (List<String>) post.get("likes");
+            if(likes.contains(userId))
+                holder.likeImageView.setImageResource(R.drawable.like_on);
+            else
+                holder.likeImageView.setImageResource(R.drawable.like_off);
+                holder.numLikesTextView.setText(String.valueOf(likes.size()));
+                holder.likeImageView.setOnClickListener(view -> {
+                Databases databases = new Databases(client);
+                Handler mainHandler = new Handler(Looper.getMainLooper());
+                List<String> nuevosLikes = likes;
+                if(nuevosLikes.contains(userId))
+                    nuevosLikes.remove(userId);
+                else
+                    nuevosLikes.add(userId);
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("likes", nuevosLikes);
+                try {
+                    databases.updateDocument(
+                            getString(R.string.APPWRITE_DATABASE_ID),
+                            getString(R.string.APPWRITE_POSTS_COLLECTION_ID),
+                            post.get("$id").toString(), // documentId
+                            data, // data (optional)
+                            new ArrayList<>(), // permissions (optional)
+                            new CoroutineCallback<>((result, error) -> {
+                                if (error != null) {
+                                    error.printStackTrace();
+                                    return;
+                                }
+                                System.out.println("Likes actualizados:" +
+                                        result.toString());
+                                mainHandler.post(() -> obtenerPosts());
+                            })
+                    );
+                } catch (AppwriteException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        @Override
+        public int getItemCount() {
+            return lista == null ? 0 : lista.getDocuments().size();
+        }
+        public void establecerLista(DocumentList<Map<String,Object>> lista)
+        {
+            this.lista = lista;
+            notifyDataSetChanged();
+        }
+    }
+
+    void obtenerPosts()
+    {
+        Databases databases = new Databases(client);
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        try {
+            databases.listDocuments(
+                    getString(R.string.APPWRITE_DATABASE_ID), // databaseId
+                    getString(R.string.APPWRITE_POSTS_COLLECTION_ID), // collectionId
+                    new ArrayList<>(), // queries (optional)
+                    new CoroutineCallback<>((result, error) -> {
+                        if (error != null) {
+                            Snackbar.make(requireView(), "Error al obtener los posts: " + error.toString(), Snackbar.LENGTH_LONG).show();
+                            return;
+                        }
+                        System.out.println( result.toString() );
+                        mainHandler.post(() -> adapter.establecerLista(result));
+                    })
+            );
+        } catch (AppwriteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/socialapp/viewholder_post.java
@@ -0,0 +1,62 @@
+package com.example.socialapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class viewholder_post extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public static viewholder_post newInstance(String param1, String param2) {
+        viewholder_post fragment = new viewholder_post();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    class PostViewHolder extends RecyclerView.ViewHolder{
+        ImageView authorPhotoImageView, likeImageView, mediaImageView;
+        TextView authorTextView, contentTextView, numLikesTextView;
+        PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            authorPhotoImageView = itemView.findViewById(R.id.photoImageView);
+            likeImageView = itemView.findViewById(R.id.likeImageView);
+            mediaImageView = itemView.findViewById(R.id.mediaImage);
+            authorTextView = itemView.findViewById(R.id.authorTextView);
+            contentTextView = itemView.findViewById(R.id.contentTextView);
+            numLikesTextView = itemView.findViewById(R.id.numLikesTextView);
+        }
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_viewholder_post, container, false);
+    }
+}
